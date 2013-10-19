74cd217272dcaf9175a198b8e880b701a6765f4c
vaqqas <m.vaqqas@gmail.com>
2013-10-19 19:12:05 +0530

    html in resonse

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send('<!DOCTYPE html><html><meta charset="UTF-8"><head><title>Carrom!!</title><body><b>Hello World!</b></body></html>');
 });
 
 var port = process.env.PORT || 5000;
